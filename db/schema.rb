8e9c9a871bf188630bf2a212266bd3d676bb007d
Anton Kosolapov <kosolapov@netpost.ru>
2016-12-08 16:05:08 +0700

    +: 13 add start, end station to ticket

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161208083035) do
+ActiveRecord::Schema.define(version: 20161208084709) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "title"
@@ -35,14 +35,18 @@ ActiveRecord::Schema.define(version: 20161208083035) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.integer  "train_id"
+    t.integer  "start_station_id"
+    t.integer  "end_station_id"
+    t.index ["end_station_id"], name: "index_tickets_on_end_station_id"
+    t.index ["start_station_id"], name: "index_tickets_on_start_station_id"
     t.index ["train_id"], name: "index_tickets_on_train_id"
     t.index ["user_id"], name: "index_tickets_on_user_id"
   end
 
   create_table "trains", force: :cascade do |t|
     t.string   "number"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer  "route_id"
     t.integer  "current_station_id"
     t.index ["current_station_id"], name: "index_trains_on_current_station_id"
