0973865a3e1b90c5ea9075a015f1f7dfc3596810
Anton Kosolapov <kosolapov@netpost.ru>
2016-12-08 10:53:48 +0700

    +: 13 Association station trains

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161208030914) do
+ActiveRecord::Schema.define(version: 20161208032141) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "title"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20161208030914) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "route_id"
+    t.integer  "current_station_id"
+    t.index ["current_station_id"], name: "index_trains_on_current_station_id"
     t.index ["route_id"], name: "index_trains_on_route_id"
   end
 
