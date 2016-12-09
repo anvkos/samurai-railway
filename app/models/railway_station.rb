f47b18165b1c63fad6b24a19872863079ead8acc
Anton Kosolapov <anvkos@ya.ru>
2016-12-09 21:47:41 +0700

    +: 14 Stations - partials, show list routes

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -2,4 +2,6 @@ class RailwayStation < ApplicationRecord
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
   has_many :trains, foreign_key: :current_station_id
+
+  validates :title, presence: true
 end
