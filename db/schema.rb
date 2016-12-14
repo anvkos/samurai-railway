106d229d31a768311e63febd1cc182bb537eef97
Anton Kosolapov <kosolapov@netpost.ru>
2016-12-14 11:24:36 +0700

    +: 16 position Route station

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161213152834) do
+ActiveRecord::Schema.define(version: 20161214030501) do
 
   create_table "carriages", force: :cascade do |t|
     t.string   "type"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20161213152834) do
   create_table "railway_stations_routes", force: :cascade do |t|
     t.integer "railway_station_id"
     t.integer "route_id"
+    t.integer "position"
   end
 
   create_table "routes", force: :cascade do |t|
