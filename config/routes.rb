fceea179f1709e09246422abbfe6d1efc77218c0
Anton Kosolapov <kosolapov@netpost.ru>
2016-12-15 11:09:08 +0700

    *: Carriage nested in Train

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
     patch :update_position, on: :member
   end
   resources :routes
-  resources :trains
-  resources :carriages
+  resources :trains do
+    resources :carriages, shallow: true
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
