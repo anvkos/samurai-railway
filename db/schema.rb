eae09782d6e6bdc473808a1b4b039e511f3cc87f
Anton Kosolapov <anvkos@ya.ru>
2016-12-19 23:17:44 +0700

    +: 18 Authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161218095025) do
+ActiveRecord::Schema.define(version: 20161219153407) do
 
   create_table "carriages", force: :cascade do |t|
     t.string   "type"
@@ -77,6 +77,18 @@ ActiveRecord::Schema.define(version: 20161218095025) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
