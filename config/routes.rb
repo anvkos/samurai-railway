c0fc36a27f0db22c40b6cfcf86ccba69638e9462
Anton Kosolapov <anvkos@ya.ru>
2016-12-21 20:59:12 +0700

    +: 18 Admin flag, move controllers, views to admin

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,19 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-  resources :railway_stations do
-    patch :update_position, on: :member
-    patch :update_time, on: :member
-  end
-  resources :routes
-  resources :trains do
-    resources :carriages, shallow: true
-  end
   resource :search, only: [:create, :show]
   resources :tickets, only: [:new, :create, :show]
+
+  namespace :admin do
+    resources :railway_stations do
+      patch :update_position, on: :member
+      patch :update_time, on: :member
+    end
+    resources :routes
+    resources :trains do
+      resources :carriages, shallow: true
+    end
+    resources :tickets
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161219153407) do
+ActiveRecord::Schema.define(version: 20161221132129) do
 
   create_table "carriages", force: :cascade do |t|
     t.string   "type"
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(version: 20161219153407) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
+    t.boolean  "admin",                  default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
