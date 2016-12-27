804f42dd7130b194a3a32a907f0437ed3068943e
Anton Kosolapov <kosolapov@netpost.ru>
2016-12-27 15:54:25 +0700

    +: 20 sending mail to buy and cancellation

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,4 +5,21 @@ class Ticket < ApplicationRecord
   belongs_to :end_station, class_name: 'RailwayStation', foreign_key: :end_station_id
 
   validates :passenger_name, :passenger_passport, presence: true
+
+  after_create :notify_buy
+  after_destroy :notify_cancellation_buy
+
+  def route_name
+    "#{start_station.title} - #{end_station.title}"
+  end
+
+  private
+
+  def notify_buy
+    TicketsMailer.buy_ticket(self.user, self).deliver_now
+  end
+
+  def notify_cancellation_buy
+    TicketsMailer.cancellation_buy_ticket(self.user, self).deliver_now
+  end
 end
